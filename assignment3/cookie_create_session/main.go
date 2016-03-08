29b2d3d5e6e64e7777c9be7e8144e595cdb2c7bb
JEVietti <dovajoe@gmail.com>
2016-03-07 18:49:24 -0800

    More of Assignment 3

--- assignment3/cookie_create_session/main.go
+++ assignment3/cookie_create_session/main.go
@@ -1,4 +1,49 @@
+//Joseph Vietti
+/*
+Create a webpage which writes a cookie to the client's machine. This cookie should be designed to create a session and should use a UUID, HttpOnly, and Secure (though you'll need to comment secure out).
+*/
+
 package main
 
+import (
+	"github.com/nu7hatch/gouuid"
+	"io"
+	"net/http"
+	"strings"
+)
+
 func main() {
+	http.Handle("/favicon.ico", http.NotFoundHandler())
+	http.HandleFunc("/", func(res http.ResponseWriter, req *http.Request) {
+		cookie, err := req.Cookie("session-id")
+		if err == http.ErrNoCookie {
+			//set the uuid for the cookie so that each user has their own session
+			id, _ := uuid.NewV4()
+			cookie = &http.Cookie{
+				Name:     "session-id",
+				Value:    id.String(),
+				HttpOnly: true,
+				//Secure:true,
+			}
+		}
+		//set the form for entering their name
+		if req.FormValue("name") != "" && !strings.Contains(cookie.Value, "name") {
+			cookie.Value = cookie.Value + `name= ` + req.FormValue("name")
+		}
+
+		http.SetCookie(res, cookie)
+		io.WriteString(res, `<!DOCTYPE html>
+		<html>
+		  <body>
+		   <form method="POST">
+		 <label for="Name">What is your name? -> </label>
+		 <input type="text" name="name">
+		 <input type="submit" value="Enter!">
+		</form>
+		  </body>
+		</html>`)
+		//check if the name and session id are being stored properly
+		io.WriteString(res,cookie.Value+", that's a good name!");
+	})
+	http.ListenAndServe(":8080", nil)
 }
--- assignment3/cookie_user_hmac/main.go
+++ assignment3/cookie_user_hmac/main.go
@@ -1,4 +1,84 @@
+//Joseph Vietti
+/*
+Create a webpage which writes a cookie to the client's machine. Though this is NOT A BEST PRACTICE, you will store some session data in the cookie. Make sure you use HMAC to ensure that session data is not changed by a user.
+*/
+
 package main
 
+import (
+	"crypto/hmac"
+	"crypto/sha256"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"strings"
+)
+
 func main() {
+	http.Handle("/favicon.ico", http.NotFoundHandler())
+	http.HandleFunc("/", func(res http.ResponseWriter, req *http.Request) {
+		cookie, err := req.Cookie("session-id")
+		if err == http.ErrNoCookie {
+			cookie = &http.Cookie{
+				Name:     "session-id",
+				Value:    "",
+				HttpOnly: true,
+				//Secure:true,
+			}
+		}
+		//set the form for entering their name
+		if req.FormValue("name") != "" {
+			cookie.Value = cookie.Value + `name= ` + getSHA("name")
+		}
+
+		http.SetCookie(res, cookie)
+		io.WriteString(res, `<!DOCTYPE html>
+		<html>
+		  <body>
+		   <form method="POST">
+		 <label for="Name">What is your name? -> </label>
+		 <input type="text" name="name">
+		 <input type="submit" value="Enter!">
+		</form>
+		<a href="/auth">Valid HMAC</a>
+		  </body>
+		</html>`)
+		//check if the name and session id are being stored properly
+		io.WriteString(res, cookie.Value+", that's a good name!")
+	})
+
+	http.HandleFunc("/auth", checkAuth)
+	http.ListenAndServe(":8080", nil)
+}
+
+func getSHA(data string) string {
+	h := hmac.New(sha256.New, []byte("key"))
+	io.WriteString(h, data)
+	return fmt.Sprintf("%x", h.Sum(nil))
+}
+
+func checkAuth(res http.ResponseWriter, req *http.Request) {
+	cookie, err := req.Cookie("cookie")
+	if err != nil {
+		http.Redirect(res, req, "/", 303)
+		return
+	}
+	if cookie.Value == "" {
+		http.Redirect(res, req, "/", 303)
+		return
+	}
+
+	xs := strings.Split(cookie.Value, "|")
+	name := xs[0]
+	shaRCVD := xs[1]
+	shaCheck := getSHA(name)
+
+	if shaRCVD != shaCheck {
+		log.Println("HMAC codes don't match")
+		log.Println(shaRCVD)
+		log.Println(shaCheck)
+		http.Redirect(res, req, "/", 303)
+		return
+	}
 }
