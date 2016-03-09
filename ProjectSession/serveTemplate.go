864a9b18aa8fd56a2c450c16d2f3712228c00875
JEVietti <dovajoe@gmail.com>
2016-03-08 19:33:35 -0800

    Project Part 1

---
+++ ProjectSession/serveTemplate.go
@@ -0,0 +1,42 @@
+package main
+
+import(
+	"html/template"
+	"net/http"
+	"io/ioutil"
+)
+
+type planet struct{
+	Title string
+	Name string
+	Number int
+	Color string
+	Member string
+	Type bool
+}
+var file1 string
+
+func servePlanet(res http.ResponseWriter, req *http.Request){
+	planetX := planet{
+		Title: "Member of The Solar System",
+		Name:"Mars ",
+		Number: 4,
+		Color: "Red",
+		Member: "Planet",
+		Type: true,
+
+	}
+	tpl,_ :=template.New("Name").Parse(file1)
+	tpl.Execute(res, planetX)
+}
+
+func init(){
+	temp,_ := ioutil.ReadFile("templates/template.html")
+	file1 = string(temp)
+}
+
+func main (){
+	http.HandleFunc("/",servePlanet)
+	http.ListenAndServe(":8080",nil)
+
+}
